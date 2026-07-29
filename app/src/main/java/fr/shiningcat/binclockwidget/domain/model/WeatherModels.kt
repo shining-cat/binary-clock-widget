bfc858522a5dee75a497f82164a970fd7ad39270
shiva <shiva.bernhard@shining-cat.fr>
2026-07-29 13:23:14 +0200

    style: reflow WeatherModels enum and data class one-per-line

--- app/src/main/java/fr/shiningcat/binclockwidget/domain/model/WeatherModels.kt
+++ app/src/main/java/fr/shiningcat/binclockwidget/domain/model/WeatherModels.kt
@@ -1,12 +1,25 @@
 package fr.shiningcat.binclockwidget.domain.model
 
 enum class DayNight { DAY, NIGHT }
-enum class WeatherCondition { CLEAR, PARTLY_CLOUDY, OVERCAST, FOG, DRIZZLE, RAIN,
-                              SNOW, RAIN_SHOWERS, SNOW_SHOWERS, THUNDERSTORM, UNKNOWN }
+enum class WeatherCondition {
+    CLEAR,
+    PARTLY_CLOUDY,
+    OVERCAST,
+    FOG,
+    DRIZZLE,
+    RAIN,
+    SNOW,
+    RAIN_SHOWERS,
+    SNOW_SHOWERS,
+    THUNDERSTORM,
+    UNKNOWN
+}
 
 /** One resolved weather picture for the widget. */
 data class WeatherSnapshot(
-    val nowCode: Int, val nowIsDay: Boolean,
-    val todayCode: Int, val tomorrowCode: Int,
+    val nowCode: Int,
+    val nowIsDay: Boolean,
+    val todayCode: Int,
+    val tomorrowCode: Int,
     val fetchedAtEpochMs: Long,
 )
